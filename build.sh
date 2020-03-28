fe8fd1cce1d5c5dc2462cd08c11736733036c851
Michael Vlach <vlach.michael@gmail.com>
2020-03-28 22:33:48 +0100

    Quote the paths

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-"clang++-10" --version
-"clang++-10" ./projects/ACore/Variant.cpp -I./projects/ACore/include/ACore -c -o Variant.cpp.o
+"clang++-10" "./projects/ACore/Variant.cpp" -I"./projects/ACore/include/ACore" -c -o Variant.cpp.o
 "llvm-ar-10" rc ACore.lib Variant.cpp.o
-"clang++-10" ./projects/ACore/test/main.cpp -I./projects/Catch2/include -c -o main.cpp.o
-"clang++-10" ./projects/ACore/test/VariantTest.cpp -I./projects/Catch2/include -I./projects/ACore/include/ -c -o VariantTest.cpp.o
+"clang++-10" "./projects/ACore/test/main.cpp" -I"./projects/Catch2/include -c -o main.cpp.o"
+"clang++-10" "./projects/ACore/test/VariantTest.cpp" -I"./projects/Catch2/include" -I"./projects/ACore/include/" -c -o VariantTest.cpp.o
 "clang++-10" main.cpp.o VariantTest.cpp.o ACore.lib -o ACoreTest
