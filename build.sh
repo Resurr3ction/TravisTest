a0edb6571b3ec41fa74fbbec8bea58b971268527
Michael Vlach <vlach.michael@gmail.com>
2020-03-28 22:15:02 +0100

    Use latest LLVM in tool names

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-clang++ --version
-clang++ ./projects/ACore/Variant.cpp -I./projects/ACore/include/ACore -c -o Variant.cpp.o
-llvm-ar rc ACore.lib Variant.cpp.o
-clang++ ./projects/ACore/test/main.cpp -I./projects/Catch2/include -c -o main.cpp.o
-clang++ ./projects/ACore/test/VariantTest.cpp -I./projects/Catch2/include -I./projects/ACore/include/ -c -o VariantTest.cpp.o
-clang++ main.cpp.o VariantTest.cpp.o ACore.lib -o ACoreTest
+clang++-11 --version
+clang++-11 ./projects/ACore/Variant.cpp -I./projects/ACore/include/ACore -c -o Variant.cpp.o
+llvm-ar-11 rc ACore.lib Variant.cpp.o
+clang++-11 ./projects/ACore/test/main.cpp -I./projects/Catch2/include -c -o main.cpp.o
+clang++-11 ./projects/ACore/test/VariantTest.cpp -I./projects/Catch2/include -I./projects/ACore/include/ -c -o VariantTest.cpp.o
+clang++-11 main.cpp.o VariantTest.cpp.o ACore.lib -o ACoreTest
